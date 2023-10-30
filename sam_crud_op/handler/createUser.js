840ffdabec15855a6ad09f7ae1e6bee023cf16a9
Amit Kumar <amit@aegiscovenant.com>
2023-10-30 10:51:13 +0000

    updating

--- sam_crud_op/handler/createUser.js
+++ sam_crud_op/handler/createUser.js
@@ -1,12 +1,22 @@
 const AWS = require("aws-sdk")
 
 const USER_TABLE = process.env.USER_TABLE;
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const END_POINT = process.env.END_POINT;
+const REGION_NAME = process.env.REGION_NAME;
+const ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+const SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient({
+    endpoint: END_POINT,
+});
+
+
 const uuid = require("uuid");
 
 
 exports.createUser = async (event, context)=>{
-    const timeStamp = new Date().getTime();
+    // const timeStamp = new Date().getTime();
     const data = JSON.parse(event.body);
 
     let body;
@@ -18,21 +28,18 @@ exports.createUser = async (event, context)=>{
 
     const params = {
         TableName : USER_TABLE,
-        Items:{
-            id: uuid.v1(),
-            userName: data.username,
-            createdAt: timeStamp,
-            updatedAt: timeStamp
+        Item:{
+            id: {S: uuid.v1()},
+            userName: {S: data.username},
+            age:{S: data.age},
         }
     }
 
-    if(typeof data !== 'string'){
-        console.log("validation failed");
-        return;
-    }
+    console.log(END_POINT, REGION_NAME, USER_TABLE)
+    console.log(typeof data, data);
 
     try{
-        body = await dynamoDB.put((params)).promise();
+        body = await dynamoDB.put(params).promise();
     }catch(err){
         statusCode = 400;
         body = err.message;
--- sam_crud_op/handler/listUsers.js
+++ sam_crud_op/handler/listUsers.js
@@ -1,34 +1,46 @@
 const AWS = require("aws-sdk");
+
 const USER_TABLE = process.env.USER_TABLE;
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const END_POINT = process.env.END_POINT;
+const REGION_NAME = process.env.REGION_NAME;
+const ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+const SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 
-exports.listUsers = async (event, context)=>{
+const dynamoDB = new AWS.DynamoDB.DocumentClient({
+    endpoint: 'http://localhost:8000',
+});
 
+exports.listUsers = async (event, context) => {
     let body;
     let statusCode;
 
+    console.log("Inside the function");
+    console.log("USER_TABLE:", USER_TABLE);
+    console.log("REGION_NAME:", REGION_NAME);
+    console.log("END_POINT:", END_POINT, ACCESS_KEY_ID, SECRET_ACCESS_KEY);
+
     const headers = {
-        "Content-type":"applcation/json",
+        "Content-Type": "application/json",
     };
 
     const params = {
-        TableName : USER_TABLE,
-    }
+        TableName: USER_TABLE,
+    };
 
-    try{
-        body = await dynamoDB.scan(params).primise();
-    }catch(err){
+    try {
+        const data = await dynamoDB.scan(params).promise();
+        console.log("Scan result:", data);
+        body = JSON.stringify(data);
+        statusCode = 200;
+    } catch (err) {
+        console.error("Error:", err);
         statusCode = 400;
-        body = err.message;
-        console.log(err);
-    }finally{
-        body = JSON.stringify(body);
+        body = JSON.stringify({ error: err.message });
     }
 
     return {
         statusCode,
         body,
         headers
-    }
-
+    };
 }
