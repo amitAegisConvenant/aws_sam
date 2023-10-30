f4f0eee1b526439c85e8279397cacef401959324
Amit Kumar <amit@aegiscovenant.com>
2023-10-30 12:42:13 +0000

    updating repo with correct code

--- sam_crud_op/handler/createUser.js
+++ sam_crud_op/handler/createUser.js
@@ -2,9 +2,7 @@ const AWS = require("aws-sdk")
 
 const USER_TABLE = process.env.USER_TABLE;
 const END_POINT = process.env.END_POINT;
-const REGION_NAME = process.env.REGION_NAME;
-const ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
-const SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+
 
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient({
@@ -29,14 +27,13 @@ exports.createUser = async (event, context)=>{
     const params = {
         TableName : USER_TABLE,
         Item:{
-            id: {S: uuid.v1()},
-            userName: {S: data.username},
-            age:{S: data.age},
+            id: uuid.v1(),
+            userName: data.username,
+            age:data.age,
         }
     }
 
-    console.log(END_POINT, REGION_NAME, USER_TABLE)
-    console.log(typeof data, data);
+    // console.log(typeof data, data);
 
     try{
         body = await dynamoDB.put(params).promise();
--- sam_crud_op/handler/listUsers.js
+++ sam_crud_op/handler/listUsers.js
@@ -2,22 +2,19 @@ const AWS = require("aws-sdk");
 
 const USER_TABLE = process.env.USER_TABLE;
 const END_POINT = process.env.END_POINT;
-const REGION_NAME = process.env.REGION_NAME;
-const ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
-const SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient({
-    endpoint: 'http://localhost:8000',
+    endpoint: END_POINT,
 });
 
+
+
 exports.listUsers = async (event, context) => {
+      
     let body;
     let statusCode;
 
     console.log("Inside the function");
-    console.log("USER_TABLE:", USER_TABLE);
-    console.log("REGION_NAME:", REGION_NAME);
-    console.log("END_POINT:", END_POINT, ACCESS_KEY_ID, SECRET_ACCESS_KEY);
 
     const headers = {
         "Content-Type": "application/json",
